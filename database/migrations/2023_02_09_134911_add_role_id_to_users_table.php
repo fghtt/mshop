3bb598db5a0abc54266827f7683535436024fad4
fghtt <usmanov4farrukh@gmail.com>
2023-02-10 19:56:36 +0700

    add migrations

---
+++ database/migrations/2023_02_09_134911_add_role_id_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->index('role_id', 'user_role_idx');
+
+            $table->foreign('role_id', 'user_role_fk')->on('roles')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+            $table->dropForeign('user_role_fk');
+            $table->dropIndex('user_role_idx');
+        });
+    }
+};
